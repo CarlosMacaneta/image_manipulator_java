8efb964ade526ddf5decbcf8d76d30fd99d8b181
Carlos Macaneta <carlosmacaneta@hotmail.com>
2022-09-28 14:42:06 +0200

    testing controller methods

--- src/image/manipulator/views/Main.java
+++ src/image/manipulator/views/Main.java
@@ -1,40 +1,38 @@
 package image.manipulator.views;
 
+import image.manipulator.controller.ImageController;
 import image.manipulator.model.Image;
 
 import javax.swing.*;
 import java.io.IOException;
 
-public class Main extends JPanel {
+public class Main {
 
     public static void main(String[] args) {
-        try {
-            JFrame frame = new JFrame("Testing img");
-            frame.setSize(600,600);
+        JFrame frame = new JFrame("Image Editor");
+        frame.setSize(600, 600);
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
 
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel();
+        ImageIcon icon = new ImageIcon();
+
+        try {
             Image img = new Image();
             img.setImgPath("C:\\Users\\dell\\Downloads\\uem.png");
-
-            ImgEditor editor = new ImgEditor(img);
-            editor.rotateImg(25);
-            editor.translateImg(300, 100);
-            editor.scaleImg(0.25, 0.25);
-
-            frame.add(editor);
-            frame.setVisible(true);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        /*try {
-            Image img = new Image();
-            img.setImgPath("C:\\Users\\dell\\Pictures\\20201212_120405.jpg");
             ImageController ic = new ImageController(img);
 
-            ic.setImgToGrayScale();
-            ic.saveImg("C:\\Users\\dell\\Pictures\\20201212_120405_copy");
+            icon.setImage(ic.rotateImage(90));
+            label.setIcon(icon);
+            label.setBounds(300, 300, icon.getIconWidth(), icon.getIconHeight());
+
+            panel.add(label);
+            panel.setBounds(0, 0, 600, 600);
+            frame.add(panel);
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
     }
 }
