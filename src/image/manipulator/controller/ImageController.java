96117576f61bdcb64fd3ef426e913da001751f72
Carlos Macaneta <56470814+CarlosMacaneta@users.noreply.github.com>
2022-10-16 21:59:57 +0200

    Editorapp controller (#1)
    * created methods for:
    1. set image to grayscale
    2. set image with negative colors
    3. set image translation
    4. rotate image
    5. scale image
    6. save edited image
    
    * testing controller methods
    
    * update
    
    * updating image controller methods
    
    * test frame update
    
    * implementing gui for editor
    
    * implementing gitignore
    
    * generated classes

--- src/image/manipulator/controller/ImageController.java
+++ src/image/manipulator/controller/ImageController.java
@@ -1,31 +1,20 @@
 package image.manipulator.controller;
 
-import image.manipulator.model.Image;
-
-import javax.imageio.ImageIO; 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 
-/**
- *
- * @author Carlos Macaneta
- */
+
 public class ImageController implements Serializable {
 
-    private BufferedImage bf;
+    //private ;
 
-    public ImageController(Image img) throws IOException {
-        bf = ImageIO.read(new File(img.getImgPath()));
-    }
-
-    public void loadImg(Image img) throws IOException {
-        bf = ImageIO.read(new File(img.getImgPath()));
-    }
-
-    public void setImageDarker() {
+    public BufferedImage setImageDarker(BufferedImage bf) {
         for (int i = 0; i < bf.getWidth(); i++) {
             for (int j = 0; j < bf.getHeight(); j++) {
                 Color oldColor = new Color(bf.getRGB(i, j));
@@ -37,9 +26,10 @@ public class ImageController implements Serializable {
                 bf.setRGB(i, j, newColor.getRGB());
             }
         }
+        return bf;
     }
 
-    public void setImgToNegative() {
+    public BufferedImage setImgToNegative(BufferedImage bf) {
         for (int i = 0; i < bf.getWidth(); i++) {
             for (int j = 0; j < bf.getHeight(); j++) {
                 Color oldColor = new Color(bf.getRGB(i, j));
@@ -51,9 +41,10 @@ public class ImageController implements Serializable {
                 bf.setRGB(i, j, newColor.getRGB());
             }
         }
+        return bf;
     }
 
-    public void setImgToGrayScale() {
+    public BufferedImage setImgToGrayScale(BufferedImage bf) {
         for (int i = 0; i < bf.getWidth(); i++) {
             for (int j = 0; j < bf.getHeight(); j++) {
                 Color oldColor = new Color(bf.getRGB(i, j));
@@ -64,11 +55,111 @@ public class ImageController implements Serializable {
                 bf.setRGB(i, j, newColor.getRGB());
             }
         }
+
+        return bf;
     }
 
+    public BufferedImage imageTranslation(BufferedImage bf) {
+        BufferedImage mirror = new BufferedImage(bf.getWidth()*2, bf.getHeight(), BufferedImage.TYPE_INT_ARGB);
 
+        for (int j = 0; j < bf.getHeight(); j++) {
+            for (int start = 0, end = bf.getWidth() * 2 - 1; start < bf.getWidth(); start++, end--) {
+                int pixel = bf.getRGB(start, j);
 
-    public void saveImg(String filename) throws IOException {
-        ImageIO.write(bf, "JPG", new File(filename+".jpg"));
+                mirror.setRGB(start, j, pixel);
+                mirror.setRGB(end, j, pixel);
+            }
+        }
+        
+        bf = mirror;
+        
+        return bf;
+    }
+
+    /*public BufferedImage rotateImage(double angDeg) {
+        int widthOfImage = bf.getWidth();
+        int heightOfImage = bf.getHeight();
+        int typeOfImage = bf.getType();
+
+        BufferedImage newImageFromBuffer = new BufferedImage(widthOfImage, heightOfImage, typeOfImage);
+
+        Graphics2D graphics2D = newImageFromBuffer.createGraphics();
+
+        graphics2D.rotate(Math.toRadians(angDeg), (double) widthOfImage / 2,  (double) heightOfImage / 2);
+        graphics2D.drawImage(bf, null, 0, 0);
+
+        bf = null;
+        bf = newImageFromBuffer;
+        return bf;
+        //return bf;
+    }*/
+
+    public BufferedImage rotateImage(
+            BufferedImage bf,
+            double angDeg,
+            int tx,
+            int ty
+    ) {
+        int widthOfImage = bf.getWidth();
+        int heightOfImage = bf.getHeight();
+        int typeOfImage = bf.getType();
+
+        BufferedImage newImageFromBuffer = new BufferedImage(widthOfImage, heightOfImage, typeOfImage);
+
+        /*Graphics2D g2d = newImageFromBuffer.createGraphics();
+
+        g2d.translate(tx/2, ty/2);
+        g2d.rotate(Math.toRadians(angDeg*3.6));
+        g2d.drawImage(bf, -widthOfImage/2, -heightOfImage/2, null);
+        g2d.dispose();*/
+
+        AffineTransform transform = new AffineTransform();
+        transform.rotate(Math.toRadians(angDeg), (double) bf.getWidth()/2, (double) bf.getHeight()/2);
+        double offset = (double)(bf.getWidth()-bf.getHeight())/2;
+        transform.translate(offset,offset);
+
+        AffineTransformOp op = new AffineTransformOp(transform, AffineTransformOp.TYPE_BILINEAR);
+        op.filter(bf, newImageFromBuffer);
+
+        return newImageFromBuffer;
+    }
+
+    public BufferedImage scaleImage(BufferedImage bf, double sx, double sy) {
+        final int w = bf.getWidth();
+        final int h = bf.getHeight();
+
+        BufferedImage scaledImage = new BufferedImage((int)sx,(int)sy, bf.getType());
+        Graphics2D g2d = scaledImage.createGraphics();
+        g2d.addRenderingHints(new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY));
+        g2d.drawImage(bf, 0, 0,w, h, null);
+        g2d.dispose();
+
+        bf = scaledImage;
+        
+        return bf;
+    }
+
+    /**
+     * Crop Image method
+     * @param bf
+     * @param x - the X coordinate of the upper-left corner of the specified rectangular region
+     * @param y - the Y coordinate of the upper-left corner of the specified rectangular region
+     * @param w - the width of the specified rectangular region
+     * @param h - the height of the specified rectangular region
+     * @param image - original image
+     * @return
+     */
+    public BufferedImage cropImg(BufferedImage bf, int x, int y, int w, int h, BufferedImage image) {
+        try {
+            bf = image.getSubimage(x, y, w, h);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return bf;
+    }
+
+    public void saveImg(BufferedImage img, String filename) throws IOException {
+        ImageIO.write(img, "PNG", new File(filename+".png"));
     }
 }
--- src/image/manipulator/views/Main.java
+++ src/image/manipulator/views/Main.java
@@ -1,50 +1,33 @@
 package image.manipulator.views;
 
-import image.manipulator.model.Image;
+import image.manipulator.controller.ImageController;
 
 import javax.swing.*;
 import java.io.IOException;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.io.File;
-import javax.imageio.ImageIO;
 
-public class Main extends JPanel {
+public class Main {
 
     public static void main(String[] args) {
-        try {
-            JFrame frame = new JFrame("Testing img");
-            frame.setSize(600,600);
-            
-            Image img = new Image();
-            img.setImgPath("C:\\Users\\Jaime Rungo\\Pictures\\R.jpg");
-            BufferedImage originalImg = ImageIO.read(
-                new File("C:\\Users\\Jaime Rungo\\Pictures\\12.jpg"));
-            
-            ImgEditor editor = new ImgEditor(img);
-            //editor.rotateImg(25);
-            //editor.translateImg(300, 100);
-            //editor.scaleImg(0.25, 0.25);
-            BufferedImage newImage = editor.cropImg(1000, 1000, 1000, 1000, originalImg);
-            img.saveImage(newImage, "C:\\Users\\Jaime Rungo\\Pictures\\1255.jpg");
-            
-           // img.saveImage(editor.cropImg(20, 50, 20, 50, originalImg), "C:\\Users\\Jaime Rungo\\Pictures\\f.jpg");
-            frame.add(editor);
-            frame.setVisible(true);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        JFrame frame = new JFrame("Image Editor");
+        frame.setSize(600, 600);
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true);
 
-        /*try {
-            Image img = new Image();
-            img.setImgPath("C:\\Users\\dell\\Pictures\\20201212_120405.jpg");
-            ImageController ic = new ImageController(img);
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel();
+        ImageIcon icon = new ImageIcon();
 
-            ic.setImgToGrayScale();
-            ic.saveImg("C:\\Users\\dell\\Pictures\\20201212_120405_copy");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
+        /*Image img = new Image();
+        img.setImgPath("C:\\Users\\dell\\Downloads\\uem.png");*/
+        ImageController ic = new ImageController();
+
+        //icon.setImage(ic.rotateImage(90));
+        label.setIcon(icon);
+        label.setBounds(300, 300, icon.getIconWidth(), icon.getIconHeight());
+
+        panel.add(label);
+        panel.setBounds(0, 0, 600, 600);
+        frame.add(panel);
     }
 }
